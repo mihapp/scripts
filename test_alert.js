27ef3841f8b494c43c76165571a703118437fdd5
mihapp <80688878+mihapp@users.noreply.github.com>
2025-07-29 12:58:50 +0200

    Update test_alert.js

--- test_alert.js
+++ test_alert.js
@@ -6,9 +6,16 @@ const apiUrl = "https://script.google.com/macros/s/AKfycbyBBNH69t_VY_fnG2WcyWRXR
 
     const h3 = document.createElement("h3");
     h3.id = "googleSheetDataId"
-    h3.textContent = "Načítám data..."
+    h3.textContent = "="
+    
+    let dots = 0;
+  setInterval(() => {
+    dots = (dots + 1) % 4; // 0,1,2,3,0,1,2,3...
+    h3.textContent = "Načítám data" + ".".repeat(dots);
+  }, 300); // každých 300 ms
+    
     textyDiv.parentNode.insertBefore(h3, textyDiv);
-    textyDiv.hidden = true; 
+    textyDiv.style.opacity = "0.5"; 
   
   fetch(apiUrl)
     .then(response => {
@@ -18,20 +25,21 @@ const apiUrl = "https://script.google.com/macros/s/AKfycbyBBNH69t_VY_fnG2WcyWRXR
       return response.text();
     })
     .then(text => {
-      const h3 = document.createElement("h3");
       h3.textContent = "Odpověď: " + text;
       h3.style.color = "red";
       //document.body.appendChild(h3);
-      textyDiv.hidden = false; 
+      textyDiv.style.opacity = "1.0"; 
+
     })
     .catch(err => {
       h3.style.color = "orange";
       h3.textContent = "⚠️ Chyba při načítání: " + err.message;
       //document.body.appendChild(h3);
       console.error("Detail chyby:", err);
-      textyDiv.hidden = false;  
-
+      textyDiv.style.opacity = "1.0";  
     });
   
   }
+
+
 
