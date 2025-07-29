6e88b65aa7b56b7eba97bb6092583fd8bd501fd2
mihapp <80688878+mihapp@users.noreply.github.com>
2025-07-29 13:03:31 +0200

    Update test_alert.js

--- test_alert.js
+++ test_alert.js
@@ -1,7 +1,6 @@
 var path = window.location.pathname;
 if (path === '/liturgicke_texty_dnes.php') {
 const apiUrl = "https://script.google.com/macros/s/AKfycbyBBNH69t_VY_fnG2WcyWRXRQol2c8YP5knewjPJoG3_sRhOop6Sr-QyLC833vdcAUo/exec";
-
     const textyDiv = document.getElementById("textyd");
 
     const h3 = document.createElement("h3");
@@ -9,7 +8,7 @@ const apiUrl = "https://script.google.com/macros/s/AKfycbyBBNH69t_VY_fnG2WcyWRXR
     h3.textContent = "="
     
     let dots = 0;
-  setInterval(() => {
+  const intervalId = setInterval(() => {
     dots = (dots + 1) % 4; // 0,1,2,3,0,1,2,3...
     h3.textContent = "Načítám data" + ".".repeat(dots);
   }, 300); // každých 300 ms
@@ -25,15 +24,17 @@ const apiUrl = "https://script.google.com/macros/s/AKfycbyBBNH69t_VY_fnG2WcyWRXR
       return response.text();
     })
     .then(text => {
-      h3.textContent = "Odpověď: " + text;
+      clearInterval(intervalId);
+      h3.textContent = "Pozor, v tento den slavíme: " + text;
       h3.style.color = "red";
       //document.body.appendChild(h3);
       textyDiv.style.opacity = "1.0"; 
 
     })
     .catch(err => {
+      clearInterval(intervalId);
       h3.style.color = "orange";
-      h3.textContent = "⚠️ Chyba při načítání: " + err.message;
+      h3.textContent = "⚠️ Nepodařilo se načíst aktuální program pro dnešní den. Kontaktujte administrátora webových stránek Vratislavice Nad Nisou: " + err.message;
       //document.body.appendChild(h3);
       console.error("Detail chyby:", err);
       textyDiv.style.opacity = "1.0";  
