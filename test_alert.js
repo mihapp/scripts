95bd1a57999b917a73f6de9554d690d7cfe4a2ed
mihapp <80688878+mihapp@users.noreply.github.com>
2025-07-29 12:32:54 +0200

    Update test_alert.js

--- test_alert.js
+++ test_alert.js
@@ -3,8 +3,12 @@ if (path === '/liturgicke_texty_dnes.php') {
 const apiUrl = "https://script.google.com/macros/s/AKfycbyBBNH69t_VY_fnG2WcyWRXRQol2c8YP5knewjPJoG3_sRhOop6Sr-QyLC833vdcAUo/exec";
 
     const textyDiv = document.getElementById("textyd");
-    textyDiv.style.display = "none"; 
 
+    const h3 = document.createElement("h3");
+    h3.id = "googleSheetDataId"
+    h3.textContent = "Načítám data..."
+    textyDiv.parentNode.insertBefore(h3, textyDiv);
+    textyDiv.hidden = true; 
   
   fetch(apiUrl)
     .then(response => {
@@ -16,19 +20,16 @@ const apiUrl = "https://script.google.com/macros/s/AKfycbyBBNH69t_VY_fnG2WcyWRXR
     .then(text => {
       const h3 = document.createElement("h3");
       h3.textContent = "Odpověď: " + text;
+      h3.style.color = "red";
       //document.body.appendChild(h3);
-      textyDiv.style.display = "block"; 
-
-      textyDiv.parentNode.insertBefore(h3, textyDiv);
+      textyDiv.hidden = false; 
     })
     .catch(err => {
-      const h3 = document.createElement("h3");
-      h3.style.color = "red";
+      h3.style.color = "orange";
       h3.textContent = "⚠️ Chyba při načítání: " + err.message;
       //document.body.appendChild(h3);
       console.error("Detail chyby:", err);
-      textyDiv.style.display = "block"; 
-      textyDiv.parentNode.insertBefore(h3, textyDiv);
+      textyDiv.hidden = false;  
 
     });
   
